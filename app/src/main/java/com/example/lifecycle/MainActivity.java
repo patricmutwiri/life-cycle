64ef3c91be39e7557d3acfbf155bb9ba7fd7388e
Patrick Mutwiri <patwiri@gmail.com>
2020-02-18 19:48:53 +0300

    Run it

--- app/src/main/java/com/example/lifecycle/MainActivity.java
+++ app/src/main/java/com/example/lifecycle/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.lifecycle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.app.Activity;
+import android.util.Log;
+import android.view.View;i
+import com.example.test.test.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    String msg = "Android : ";
+    private Button btnExit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnExit = (Button) findViewById(R.id.button);
+        Log.d(msg, "The onCreate() event");
+//set GUI listeners, watchers,...
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event");
+    }
+    /** Called when the activity has become visible. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event");
+    }
+    /** Called when another activity is taking focus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event");
+    }
+    /** Called when the activity is no longer visible. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event");
+    }
+    /** Called just before the activity is destroyed. */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event");
     }
 }
